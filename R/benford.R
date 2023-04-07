a9fff0fe2bd3d4b7635dbaaecb0bd96f4b2696c3
Marcelo S. Camacho <marcelocamacho.ufpa@gmail.com>
2023-04-07 20:15:10 -0300

    Função benford que retorna uma lista com os parâmetros da resposta

---
+++ R/benford.R
@@ -0,0 +1,18 @@
+benford <-function(x) {
+ freq_obs <- table(substr(as.character(x),1,1))
+ N <- sum(freq_obs)
+ dig_obs <- as.numeric(names(freq_obs))
+ Num_dig <- length(dig_obs)
+ prop_esp <- log10(1 + 1 / dig_obs)
+ freq_esp <- prop_esp * N
+ estatistica <- sum((freq_esp - freq_obs) ^ 2 / freq_esp)
+ p_val <- 1 - pchisq(estatistica, Num_dig -1)
+ res <- list(estatistica = estatistica,
+             p_val = p_val,
+             freq_obs = as.numeric(freq_obs),
+             prop_esp = prop_esp,
+             prop_obs = freq_obs / N
+        )
+ class(res) <- 'benford'
+ res
+}
