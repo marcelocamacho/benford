ca32f590c8c0ba0958b1c6e2b3bf2f1d3f1d7234
Marcelo S. Camacho <marcelocamacho.ufpa@gmail.com>
2023-04-07 20:15:28 -0300

    Verifica se o objeto retornado está correto

---
+++ tests/testthat/test-benford.R
@@ -0,0 +1,9 @@
+test_that("retorne um objeto da classe benford", {
+  x<-sample(1:1000,100)
+  testthat::expect_s3_class(benford(x),'benford')
+})
+
+test_that("retorne um objeto do tipo lista", {
+ x<-sample(1:1000,100)
+ testthat::expect_type(benford(x),'list')
+})
